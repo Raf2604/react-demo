2b36fed8eb4df31a0480b9de5fe7bd33954976c7
Rafayel <rafsahakyan2604@gmail.com>
2021-01-21 17:02:30 +0400

    using fontawesome, add some buttons and modal

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,32 +1,34 @@
-import React, {Component} from 'react';
-import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Form, FormControl, Modal, Button } from 'react-bootstrap';
 import idGenerator from '../../helpers/idGenerator';
 import PropTypes from 'prop-types';
 
-export default class NewTask extends Component{
+export default class NewTask extends Component {
     state = {
-        title:"",
-        description:""
+        title: "",
+        description: ""
     }
 
-    inputTextChange = (event) => {
+    inputTextChange = (event, name) => {
         this.setState({
-            title: event.target.value
+            [name]: event.target.value
         })
     }
 
     addFromKeyboard = (event) => {
-        if(event.key === "Enter"){
-            this.addElem();
+        if (event.key === "Enter") {
+            this.creatTask();
         }
     }
 
     creatTask = () => {
-        const {title, description} = this.state;
-        if (!title.trim()) {
+        const title = this.state.title.trim();
+        const description = this.state.description.trim();
+
+        if (!title) {
             this.setState({
-                title:"",
-                description:""
+                title: "",
+                description: ""
             })
             return
         }
@@ -36,37 +38,51 @@ export default class NewTask extends Component{
             description: description
         }
         this.props.onAdd(newTask);
-        this.setState({
-                title:"",
-                description:""
-        })
     }
 
     render() {
-        const {disabled} = this.props;
+        const { onClose } = this.props;
         return (
-            <InputGroup className="mb-3 justify-content-center">
-                <FormControl
-                    placeholder="Enter title of task"
-                    aria-describedby="basic-addon2"
-                    onChange={this.inputTextChange}
-                    onKeyDown={this.addFromKeyboard}
-                    value={this.state.title}
-                    disabled={disabled}
-                />
-                <InputGroup.Append>
-                    <Button 
-                        disabled={disabled} 
-                        variant="outline-primary" 
-                        onClick={this.creatTask}
-                    >Add Task</Button>
-                </InputGroup.Append>
-            </InputGroup>
+                <Modal
+                    onHide={onClose}
+                    show={true}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                    >
+                    <Modal.Header closeButton >
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Add new task
+                        </Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <FormControl
+                            className={"mb-3"}
+                            placeholder="Enter title of task"
+                            aria-describedby="basic-addon2"
+                            onChange={(event)=>this.inputTextChange(event, "title")}
+                            onKeyPress={this.addFromKeyboard}
+                            name="title"
+                        />
+                        <Form.Control  
+                            as="textarea" 
+                            rows={3} 
+                            placeholder="Enter description of task"
+                            onChange={(event)=>this.inputTextChange(event, "description")}
+                            name="description"
+                        />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="success" onClick={this.creatTask}>Create</Button>
+                        <Button onClick={onClose}>Cancel</Button>
+                    </Modal.Footer>
+                    </Modal>
         )
     }
 }
 
 NewTask.propType = {
-    disabled: PropTypes.bool.isRequired,
-    onAdd: PropTypes.func.isRequired
+    onAdd: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired
 }
